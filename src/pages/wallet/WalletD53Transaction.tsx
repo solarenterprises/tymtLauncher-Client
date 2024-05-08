43da314997bc74e2fb25e8dd7d41af7a8d70d995
farhan219104 <farhanyusof219104@gmail.com>
2024-05-08 11:58:04 +0200

    fix: D53 transaction window style

--- src/pages/wallet/WalletD53Transaction.tsx
+++ src/pages/wallet/WalletD53Transaction.tsx
@@ -1,12 +1,47 @@
 import { Box, Button, Grid } from "@mui/material";
 import tymt from "../../assets/main/newlogo.png";
 import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../../store";
+import {
+  setMountedFalse,
+  setMountedTrue,
+} from "../../features/chat/Chat-intercomSupportSlice";
+import {
+  selectNotification,
+  setNotification,
+} from "../../features/settings/NotificationSlice";
+import { notificationType } from "../../types/settingTypes";
 
 const WalletD53Transaction = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const notificationStore: notificationType = useSelector(selectNotification);
+
+  useEffect(() => {
+    dispatch(setMountedTrue());
+    dispatch(
+      setNotification({
+        ...notificationStore,
+        alert: false,
+      })
+    );
+
+    return () => {
+      dispatch(setMountedFalse());
+      dispatch(
+        setNotification({
+          ...notificationStore,
+          alert: true,
+        })
+      );
+    };
+  }, [dispatch]);
+
   return (
     <Grid container className="basic-container">
       <Grid item xs={12} container justifyContent={"center"}>
-        <Box component="img" src={tymt} width="400px" height="300px" />
+        <Box component="img" src={tymt} width="300" height="100px" />
       </Grid>
       <Grid item xs={12}>
         <Button
