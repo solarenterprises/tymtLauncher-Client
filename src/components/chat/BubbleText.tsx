189de8cc847d1929eb872a093740c27af472932a
RAZGRIZ219104 <farhanyusof219104@gmail.com>
2024-08-13 00:02:08 +0200

    fix: display the message sender nickname in the text bubble

--- src/components/chat/BubbleText.tsx
+++ src/components/chat/BubbleText.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
 import { ThreeDots } from "react-loader-spinner";
 
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 import Linkify from "./Linkify";
+
+import { getCurrentChatroomMembers, ICurrentChatroomMembers } from "../../features/chat/CurrentChatroomMembersSlice";
 
 import { ChatMessageType } from "../../types/chatTypes";
 
@@ -16,6 +19,13 @@ export interface IParamsBubbleText {
 }
 
 const BubbleText = ({ roomMode, message, decryptedMessage, isLastMessage, isSender }: IParamsBubbleText) => {
+  const currentChatroomMembersStore: ICurrentChatroomMembers = useSelector(getCurrentChatroomMembers);
+  const displayNickname: string = useMemo(() => {
+    const sender = currentChatroomMembersStore.members.find((member) => member._id === message.sender_id);
+    if (sender) return sender.nickName;
+    return "Deleted";
+  }, [currentChatroomMembersStore]);
+
   return (
     <>
       <Box
@@ -23,24 +33,28 @@ const BubbleText = ({ roomMode, message, decryptedMessage, isLastMessage, isSend
           isLastMessage ? (roomMode ? "br-20-20-20-0" : isSender ? "br-20-20-0-20" : "br-20-20-20-0") : "br-20"
         }`}
       >
-        {decryptedMessage !== undefined ? (
-          <>
-            {decryptedMessage.split("\n").map((line, index) => (
-              <React.Fragment key={index}>
-                <Linkify>{line}</Linkify>
-                <br />
-              </React.Fragment>
-            ))}
-            <Box className="fs-14-light timestamp-inbubble" sx={{ alignSelf: "flex-end" }} color={"rgba(11, 11, 11, 0.7)"}>
-              {new Date(message.createdAt).toLocaleString("en-US", {
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </Box>
-          </>
-        ) : (
-          <ThreeDots height="23px" width={"40px"} radius={4} color={`white`} />
-        )}
+        <Stack>
+          <Box className={"fs-12-regular white"}>{displayNickname}</Box>
+          <Box>
+            {decryptedMessage !== undefined ? (
+              <>
+                {decryptedMessage.split("\n").map((line, index) => (
+                  <React.Fragment key={index}>
+                    <Linkify>{line}</Linkify>
+                  </React.Fragment>
+                ))}
+                <Box className="fs-14-light timestamp-inbubble" sx={{ alignSelf: "flex-end" }} color={"rgba(11, 11, 11, 0.7)"}>
+                  {new Date(message.createdAt).toLocaleString("en-US", {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </Box>
+              </>
+            ) : (
+              <ThreeDots height="23px" width={"40px"} radius={4} color={`white`} />
+            )}
+          </Box>
+        </Stack>
       </Box>
     </>
   );
